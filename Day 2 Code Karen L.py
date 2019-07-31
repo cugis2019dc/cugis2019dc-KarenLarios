dd0f23ea4d1d2c42c4c92e34f418ff033a853c80
KarenLarios <53440589+KarenLarios@users.noreply.github.com>
2019-07-31 10:10:26 -0400

    Add files via upload

---
+++ Day 2 Code Karen L.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Tue Jul 30 10:50:44 2019
+
+@author: STEMc
+"""
+
+def cadbury(a):
+    cadbury = a+10
+    print(cadbury)
+    
+cadbury(5)
+
+def cadbury(a):
+    cadbury = a+15
+    print(cadbury)
+    
+cadbury(5)
+
+cadburytype1= "Milk Chocalate"
+cadburytype2= "Dark Chocalate"
+cadburytype3= "White Chocalte"
+
+def cadburybox(w,d,m):
+    print("There is", m,"," ,d,",", w, " in the box")
+    
+cadburybox("White chocalate","Dark chocalate", "Milk chocalte")
+
+#greet a person by their name 
+
+print("Hello Karen")
+
+a ="Karen"
+print("hello",a)
+
+name = input("please enter your name")
+     
+print("Your name is",name)
+
+age = ("please enter your age")
+
+print("Thank you. You entered", age)    
+
+import math
+
+dir(math)
+
+math.pi
+
+math.factorial(12)
+
+math.pow(6,15)
+
+def cubicroot(a):
+    print("The cubic root of",a,"is",math.pow(a,(1/3)))
+    
+cubicroot(10)
+
+a= int(input("enter value"))
+
+
